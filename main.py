3524ed12454c035ecdadce583b44d41a0aeccc39
Amarsoftaaa <amar.softic.ets@gmail.com>
2025-01-06 00:06:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+ljudi = {
+    "osoba1" : {
+    "Ime":"Amar",
+    "Prezime":"Softic",
+"Godine":"29"
+},
+
+"osoba2": {
+"Ime":"Eldar",
+    "Prezime":"Huremovic",
+"Godine":"32"
+},
+"osoba3" :{
+"Ime":"Amar",
+    "Prezime":"Softic",
+"Godine":"29"
+}
+
+}
+print(ljudi)
